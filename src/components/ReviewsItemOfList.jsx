282b75bdce91395f696772c0e10da4552e032a12
JoshuaGoodwill <jgoodwill1234@gmail.com>
2023-05-31 10:37:56 +0100

    Changed useNavigate to a Link

--- src/components/ReviewsItemOfList.jsx
+++ src/components/ReviewsItemOfList.jsx
@@ -1,22 +1,18 @@
-import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 
 const ReviewsItemOfList = ({ review }) => {
-  const navigate = useNavigate();
-
-  const clickReview = () => {
-    const path = `/reviews/${review.review_id}`;
-    navigate(path);
-  };
-
   const fullDate = new Date(review.created_at);
 
   return (
-    <li className="reviewsListItem" onClick={clickReview}>
+    <li className="reviewsListItem">
       <h3 className="individualReviewTitle">{review.title}</h3>
       <p>Author: {review.owner}</p>
       <p>Posted: {fullDate.getFullYear()}</p>
       <p>Votes: {review.votes}</p>
       <img className="individualReviewImage" src={review.review_img_url} />
+      <Link to={`/reviews/${review.review_id}`} className="reviewLink">
+        Click To Go To Review
+      </Link>
     </li>
   );
 };
